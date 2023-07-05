3e93be0ea9dbddc41f3ad8829dbb7107cb1cd1ee
tnmydesai <tanmaydesai09@gmail.com>
2023-07-06 00:59:57 +0530

    footer added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,6 +10,7 @@ import Facilities from './components/FacilitiesFolder/Facilities';
 // import Schedule from './components/Schedule/Schedule';
 // import Registration from './components/Registration/Registration';
 // import Contact from './components/Contact/Contact';
+import Footer from './components/FooterFolder/Footer';
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
         <Route path="/coaches" component={Coaches} />
         <Route path="/programs" component={Programs} />
       </Switch>
+      <Footer />
     </Router>
   );
 };
---
+++ my-app/src/components/FooterFolder/Footer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './FtrStyle.css';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <footer className="footer expand-lg">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="social-icons">
+              <a href="https://www.facebook.com/your-username">
+                <FaFacebook />
+              </a>
+              <a href="https://www.instagram.com/your-username">
+                <FaInstagram />
+              </a>
+              <a href="https://www.twitter.com/your-username">
+                <FaTwitter />
+              </a>
+            </div>
+          </div>
+          <div className="col-md-12">
+            <ul className="footer-links">
+              <li>
+                <a href="/registration">Registration</a>
+              </li>
+              <li>
+                <a href="/contact">Contact Us</a>
+              </li>
+              <li>
+                <a href="/about">About Us</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-12">
+            <p className="footer-copy">
+              &copy; {new Date().getFullYear()} Football Academy. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
